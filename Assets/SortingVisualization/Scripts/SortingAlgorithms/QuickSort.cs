96c339a4e1e858af3ed0d84a5d76a3aac437e5b8
Cooper Saye <coopersaye@gmail.com>
2020-09-07 13:45:49 -0700

    Implement quicksort.

--- Assets/SortingVisualization/Scripts/SortingAlgorithms/QuickSort.cs
+++ Assets/SortingVisualization/Scripts/SortingAlgorithms/QuickSort.cs
@@ -14,50 +14,40 @@ namespace SortingVisualization
         public static IEnumerator Sort(SortingStacks _sortingStacks)
         {
             sortingStacks = _sortingStacks;
-
-            int pivot = GetPivotStack(sortingStacks.stacks, 0, stackCount);
-            sortingStacks.SetStack(pivot, stackCount / 2);
-            for (int i = 0; i < stackCount / 2; i++)
+            int partitionSize = stackCount;
+            while (!sortingStacks.StacksSorted() && partitionSize > 1)
             {
-                if (sortingStacks.stacks[i] < pivot)
+                for (int startIndex = 0; startIndex < stackCount; startIndex += partitionSize)
                 {
-                    for (int j = stackCount / 2; j < stackCount; j++)
+                    int endIndex = startIndex + partitionSize;
+                    if (endIndex > stackCount) endIndex = stackCount;
+                    int midIndex = (startIndex + endIndex) / 2;
+                    int pivot = GetPivotStack(sortingStacks.stacks, startIndex, endIndex);
+                    sortingStacks.SetStack(pivot, midIndex);
+                    for (int i = startIndex; i < midIndex; i++)
                     {
-                        if (pivot < sortingStacks.stacks[j]) continue;
-                        sortingStacks.SwapStackIndices(i, j);
-                        yield return new WaitForSeconds(sortDelay);
-                        break;
+                        if (sortingStacks.stacks[i] > pivot)
+                        {
+                            for (int j = midIndex + 1; j < endIndex; j++)
+                            {
+                                if (pivot < sortingStacks.stacks[j]) continue;
+                                sortingStacks.SwapStackIndices(i, j);
+                                yield return new WaitForSeconds(sortDelay);
+                                break;
+                            }
+                        }
+
                     }
                 }
+                partitionSize /= 2;
             }
-
             sortingStacks.StopSort();
         }
-
-        // private static IEnumerator Partition(int startIndex, int endIndex)
-        // {
-        //     int midIndex = (startIndex + endIndex) / 2;
-        //     int pivot = GetPivotStack(sortingStacks.stacks, startIndex, endIndex);
-        //     sortingStacks.SetStack(pivot, midIndex);
-        //     for (int i = startIndex; i < midIndex; i++)
-        //     {
-        //         if (sortingStacks.stacks[i] > pivot)
-        //         {
-        //             for (int j = midIndex + 1; j < endIndex; j++)
-        //             {
-        //                 if (sortingStacks.stacks[j] > sortingStacks.stacks[i]) continue;
-        //                 sortingStacks.SwapStackIndices(i, j);
-        //                 yield return new WaitForSeconds(sortDelay);
-        //                 break;
-        //             }
-        //         }
-        //     }
-        // }
 
         private static int GetPivotStack(int[] stacks, int startIndex, int endIndex)
         {
             int[] sortedStacks = new int[endIndex - startIndex];
-            for (int i = startIndex; i < endIndex; i++) sortedStacks[i] = stacks[i];
+            for (int i = startIndex; i < endIndex; i++) sortedStacks[i - startIndex] = stacks[i];
             Array.Sort(sortedStacks);
             return sortedStacks[sortedStacks.Length / 2];
         }
