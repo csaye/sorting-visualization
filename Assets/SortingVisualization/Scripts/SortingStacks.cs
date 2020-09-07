36e79d93041e33120766338bfcd9f187b637a0dd
Cooper Saye <coopersaye@gmail.com>
2020-09-06 19:51:46 -0700

    Implement RandomizeStacks method.

--- Assets/SortingVisualization/Scripts/SortingStacks.cs
+++ Assets/SortingVisualization/Scripts/SortingStacks.cs
@@ -1,10 +1,50 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace SortingVisualization
 {
     public class SortingStacks : MonoBehaviour
     {
-        // [Header("References")]
-        // [SerializeField] private GameObject[] stackObjects = new GameObject[64];
+        [Header("References")]
+        [SerializeField] private Transform[] stackTransforms = new Transform[stackCount];
+        [SerializeField] private int[] stackNumbers = new int[stackCount];
+
+        private const int stackCount = 64;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown("space")) RandomizeStacks();
+        }
+
+        private void RandomizeStacks()
+        {
+            int[] randomIndices = GetRandomIndices(stackCount);
+            for (int i = 0; i < stackCount; i++)
+            {
+                stackTransforms[randomIndices[i]].SetSiblingIndex(i);
+            }
+        }
+
+        private int[] GetRandomIndices(int size)
+        {
+            int[] randomIndices = new int[size];
+            for (int i = 0; i < size; i++) randomIndices[i] = -1;
+            for (int i = 0; i < size; i++)
+            {
+                int randomIndex = Random.Range(0, size);
+                while (randomIndices.Contains(randomIndex)) randomIndex = Random.Range(0, size);
+                randomIndices[i] = randomIndex;
+            }
+            return randomIndices;
+        }
+
+        private bool StacksSorted()
+        {
+            for (int i = 0; i < stackCount; i++)
+            {
+                if (stackNumbers[i] != i) return false;
+            }
+            return true;
+        }
     }
 }
