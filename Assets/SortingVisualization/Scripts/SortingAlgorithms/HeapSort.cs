5548ee016fc9739c073179b50051675ef64c0cc1
Cooper Saye <coopersaye@gmail.com>
2020-09-09 14:46:13 -0700

    Refactor initial heap sorting process.

--- Assets/SortingVisualization/Scripts/SortingAlgorithms/HeapSort.cs
+++ Assets/SortingVisualization/Scripts/SortingAlgorithms/HeapSort.cs
@@ -25,123 +25,56 @@ namespace SortingVisualization
 
             for (int i = (stackCount / 2) - 1; i >= 0; i--)
             {
-                Heapify(stackCount, i);
+                bool sorting = true;
+                int[] arr = sortingStacks.stacks;
+                int size = stackCount;
+                int index = i;
+                while (sorting)
+                {
+                    int largest = index;
+                    int left = (2 * index) + 1;
+                    int right = (2 * index) + 2;
+                    if (left < size && arr[left] > arr[largest]) largest = left;
+                    if (right < size && arr[right] > arr[largest]) largest = right;
+                    if (largest != index)
+                    {
+                        sortingStacks.SwapStackIndices(index, largest);
+                        yield return new WaitForSeconds(sortDelay);
+                        index = largest;
+                    }
+                    else sorting = false;
+                }
             }
-
             for (int i = stackCount - 1; i > 0; i--)
             {
                 sortingStacks.SwapStackIndices(0, i);
                 yield return new WaitForSeconds(sortDelay);
-                Heapify(i, 0);
+                MakeBinaryHeap(sortingStacks.stacks, i, 0);
             }
             sortingStacks.StopSort();
+            // sortingStacks = _sortingStacks;
+            // for (int i = (stackCount / 2) - 1; i >= 0; i--) MakeBinaryHeap(sortingStacks.stacks, stackCount, i);
+            // for (int i = stackCount - 1; i > 0; i--)
+            // {
+            //     sortingStacks.SwapStackIndices(0, i);
+            //     yield return new WaitForSeconds(sortDelay);
+            //     MakeBinaryHeap(sortingStacks.stacks, i, 0);
+            // }
+            // sortingStacks.StopSort();
         }
 
-        private static void Heapify(int n, int i)
+        private static void MakeBinaryHeap(int[] arr, int size, int index)
         {
-            int largest = i;
-            int left = (2 * i) + 1;
-            int right = (2 * i) + 2;
-
-            if (left < n && sortingStacks.stacks[left] > sortingStacks.stacks[largest]) largest = left;
-            if (right < n && sortingStacks.stacks[right] > sortingStacks.stacks[largest]) largest = right;
-
-            if (largest != i)
+            int largestIndex = index;
+            int leftIndex = (2 * index) + 1;
+            int rightIndex = (2 * index) + 2;
+            if (leftIndex < size && arr[leftIndex] > arr[largestIndex]) largestIndex = leftIndex;
+            if (rightIndex < size && arr[rightIndex] > arr[largestIndex]) largestIndex = rightIndex;
+            if (largestIndex != index)
             {
-                sortingStacks.SwapStackIndices(i, largest);
-                Heapify(n, largest);
+                sortingStacks.SwapStackIndices(index, largestIndex);
+                MakeBinaryHeap(arr, size, largestIndex);
             }
         }
-
-        // private static List<Node> heap = new List<Node>();
-
-        // public static IEnumerator Sort(SortingStacks _sortingStacks)
-        // {
-        //     sortingStacks = _sortingStacks;
-        //     InitializeHeap();
-        //     // LogHeap();
-        //     while (heap[0].stack != 63)
-        //     {
-        //         Debug.Log("heap at zero: " + heap[0].stack);
-        //         bool nodeSwapped = false;
-        //         for (int i = 0; i < heap.Count; i++)
-        //         {
-        //             if (heap[i].childA != null && heap[i].stack < heap[i].childA.stack)
-        //             {
-        //                 nodeSwapped = true;
-        //                 HeapSwap(heap[i], heap[i].childA);
-        //                 yield return new WaitForSeconds(sortDelay);
-        //             }
-        //             if (heap[i].childB != null && heap[i].stack < heap[i].childB.stack)
-        //             {
-        //                 nodeSwapped = true;
-        //                 HeapSwap(heap[i], heap[i].childB);
-        //                 yield return new WaitForSeconds(sortDelay);
-        //             }
-        //         }
-        //         if (!nodeSwapped) break;
-        //     }
-        //     // Debug.Log("done, node 63 at " + heap.FindIndex(node => node.stack == 63));
-        //     while (!Input.GetKeyDown("space"))
-        //     {
-        //         yield return null;
-        //     }
-        //     LogHeap();
-        //     sortingStacks.StopSort();
-        // }
-
-        // private static void LogHeap()
-        // {
-        //     foreach (Node n in heap)
-        //     {
-        //         Debug.Log(n.stack);
-        //         if (n.childA != null) Debug.Log(n.childA.stack);
-        //         if (n.childB != null) Debug.Log(n.childB.stack);
-        //         Debug.Log("----------------------");
-        //     }
-        // }
-
-        // private static void HeapSwap(Node nodeA, Node nodeB)
-        // {
-        //     Debug.Log("swapping " + nodeA.stack + " and " + nodeB.stack);
-        //     sortingStacks.SwapStacks(nodeA.stack, nodeB.stack);
-        //     int heapIndexA = heap.IndexOf(nodeA);
-        //     int heapIndexB = heap.IndexOf(nodeB);
-        //     heap[heapIndexA].stack = nodeB.stack;
-        //     heap[heapIndexB].stack = nodeA.stack;
-        //     Node temp = heap[heapIndexA];
-        //     heap[heapIndexA] = heap[heapIndexB];
-        //     heap[heapIndexB] = temp;
-        // }
-
-        // private static void InitializeHeap()
-        // {
-        //     heap = new List<Node>();
-        //     for (int i = 0; i < stackCount; i++)
-        //     {
-        //         int stackToAdd = sortingStacks.stacks[i];
-        //         Node nodeToAdd = new Node(stackToAdd);
-        //         for (int j = 0; j < stackCount; j++)
-        //         {
-        //             if (heap.Count <= j)
-        //             {
-        //                 heap.Add(nodeToAdd);
-        //                 break;
-        //             }
-        //             if (heap[j].childA == null)
-        //             {
-        //                 heap[j].childA = nodeToAdd;
-        //                 heap.Add(nodeToAdd);
-        //                 break;
-        //             }
-        //             if (heap[j].childB == null)
-        //             {
-        //                 heap[j].childB = nodeToAdd;
-        //                 heap.Add(nodeToAdd);
-        //                 break;
-        //             }
-        //         }
-        //     }
-        // }
     }
 }
